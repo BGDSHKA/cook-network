bab24672ed8315dc8ce586604c6e66bc70e33c9b
Kirill Ateev <Kirill.ateev59@gmail.com>
2020-08-06 16:28:34 +0500

    Some start files

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://c7.hotpng.com/preview/770/632/917/food-network-cooking-channel-television-recipe-food-logo.jpg'></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+      <img src='https://sun9-35.userapi.com/c855536/v855536487/25136c/i08jzIeEloU.jpg'></img>
+      <div>
+        ava + descr
+      </div>
+      <div>
+        My posts
+        <div>
+          new post
+        </div>
+        <div>
+          <div>
+            post1
+          </div>
+          <div>
+            post2
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Profile;
