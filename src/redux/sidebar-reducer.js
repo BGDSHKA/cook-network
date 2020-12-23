b2d89b0859a2179dce0b9ec880cf3763ed6559d0
BGDSHKA <kirill.ateev59@gmail.com>
2020-12-23 22:45:57 +0300

    feature pagination users page

--- src/redux/sidebar-reducer.js
+++ src/redux/sidebar-reducer.js
@@ -1,10 +1,11 @@
+import userPhoto from '../assets/images/nophoto.png';
 
 let initialState = {
     friends: [
-        { id: 1, name: 'Egor Dolganov', photo: 'https://www.sciencedebate2008.com/wp-content/uploads/chel.jpg' },
-        { id: 2, name: 'Pyotr Furniture', photo: 'https://www.sciencedebate2008.com/wp-content/uploads/chel.jpg' },
-        { id: 3, name: 'Anna Petrovna', photo: 'https://www.sciencedebate2008.com/wp-content/uploads/chel.jpg' },
-        { id: 4, name: 'Test Test', photo: 'https://www.sciencedebate2008.com/wp-content/uploads/chel.jpg' },
+        { id: 1, name: 'Egor Dolganov', photo: userPhoto },
+        { id: 2, name: 'Pyotr Furniture', photo: userPhoto },
+        { id: 3, name: 'Anna Petrovna', photo: userPhoto },
+        { id: 4, name: 'Test Test', photo: userPhoto },
     ]
 };
 
