9499e0606e5e4e4adb7dc991097935c7444d3a0b
BGDSHKA <kirill.ateev59@gmail.com>
2020-12-24 21:31:47 +0300

    feature isFetching svg

---
+++ src/common/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div>
+        <img src={"https://samherbert.net/svg-loaders/svg-loaders/tail-spin.svg"} />
+    </div>
+  );
+};
+
+export default Preloader;
