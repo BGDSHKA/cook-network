ce0e02a18b413cc8fed8a0cc51534b9db838d7d9
BGDSHKA <kirill.ateev59@gmail.com>
2020-12-26 16:53:51 +0300

    feature add DAL for API

---
+++ src/api/api.js
@@ -0,0 +1,37 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "cf731e51-f342-4292-b062-323d3b774f6a",
+      }
+});
+
+export const usersAPI = {
+
+      getUsers(currentPage, pageSize) {
+        return instance
+          .get(`users?page=${currentPage}&count=${pageSize}`)
+          .then((response) => response.data);
+      },
+      
+        auth() {
+        return instance
+          .get(`auth/me`)
+          .then((response) => response.data);
+      },
+      
+        unfollow(id) {
+        return instance
+          .delete(`follow/${id}`)
+          .then((response) => response.data);
+      },
+      
+        follow(id) {
+        return instance
+          .post(`follow/${id}`)
+          .then((response) => response.data);
+      }
+      
+}
