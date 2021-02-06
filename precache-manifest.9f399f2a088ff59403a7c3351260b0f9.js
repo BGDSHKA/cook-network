ab52a99ba866d182fc24b767bd7188c2eb029e64
BGDSHKA <kirill.ateev59@gmail.com>
2021-02-06 15:29:29 +0300

    Updates

---
+++ precache-manifest.9f399f2a088ff59403a7c3351260b0f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce106fb367a047d587360dd16f2880ea",
+    "url": "/cook-network/index.html"
+  },
+  {
+    "revision": "ef8ec7390d47784cdf80",
+    "url": "/cook-network/static/css/3.1a116b26.chunk.css"
+  },
+  {
+    "revision": "3e2a4733d160f5544cc0",
+    "url": "/cook-network/static/css/main.74227cb1.chunk.css"
+  },
+  {
+    "revision": "3a58fadf0d5c62bef153",
+    "url": "/cook-network/static/js/2.d63f34fa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cook-network/static/js/2.d63f34fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef8ec7390d47784cdf80",
+    "url": "/cook-network/static/js/3.4087818a.chunk.js"
+  },
+  {
+    "revision": "183c906b03c53087b983",
+    "url": "/cook-network/static/js/4.e79732f0.chunk.js"
+  },
+  {
+    "revision": "3e2a4733d160f5544cc0",
+    "url": "/cook-network/static/js/main.76c85011.chunk.js"
+  },
+  {
+    "revision": "758775c29514a8a8e6e4",
+    "url": "/cook-network/static/js/runtime-main.94f38eb7.js"
+  }
+]);
